99a23327adfb92fc1f76ea0ee59277a0ef92f250
bhushan2311 <bambhore350@gmail.com>
2024-03-28 23:49:33 +0530

    Footer - 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinar from "@/components/UpcomingWebinar";
 import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
-
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
       <FeaturedCourses/>
@@ -17,6 +17,7 @@ export default function Home() {
       <TestimonialCards/>
       <UpcomingWebinar/>
       <Instructors/>
+      <Footer/>
     </main>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="text-gray-200 py-12 bg-black">
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
